c6a0d079ba82a725fe31deb0b4b84c1e9251a9b8
Erisnilton Freitas <erisniltongf@gmail.com>
2022-11-10 16:14:18 -0300

    ref(animation) animation in card

--- src/components/note-view/NoteView.tsx
+++ src/components/note-view/NoteView.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Note } from "../../model/note";
 import Card from "../card/Card";
-import  { useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 
 interface NotaViewProps {
   nota: Note;
@@ -14,7 +14,7 @@ const NotaView: React.FunctionComponent<NotaViewProps> = (props) => {
 
   const handleNavigateInfo = () => {
     navigate(`notes/${nota.id}`);
-  }
+  };
   return (
     <div onClick={handleNavigateInfo}>
       <Card nota={nota}></Card>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,16 @@ import { Authenticated } from "../components/Authenticated/Authenticated";
 import { useState, useEffect, useContext } from "react";
 import NoteView from "../components/note-view/NoteView";
 import { Note } from "../model/note";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const Home: React.FunctionComponent = () => {
+  const [parent] = useAutoAnimate<HTMLDivElement>();
   const items = JSON.parse(localStorage.getItem("notas") || "[]") as Note[];
   return (
     <>
       <NavBar />
       <Layout>
-        <div className="view--nota">
+        <div className="view--nota" ref={parent}>
           {items &&
             items.map((item) => (
               <NoteView key={item.id} nota={item}></NoteView>
